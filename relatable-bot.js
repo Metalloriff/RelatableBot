c128651c22c9d43a095cd30ed3ceed68cc7dd58b
itbchannel <itbchannel@gmail.com>
2018-04-07 14:20:10 -0400

    Update

--- relatable-bot.js
+++ relatable-bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 const sql = require("sqlite");
-sql.open("./data/trigger-whitelists.sqlite");
+//sql.open("./data/trigger-whitelists.sqlite");
 
 client.on("ready", () => {
     console.log("ready, fam");
